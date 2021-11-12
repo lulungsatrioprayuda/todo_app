31425a9fe6fc4904306f54db5866e419ec6c30cf
lulungsatrioprayuda <alungsatrio12@gmail.com>
2021-11-12 13:16:23 +0700

    change sdk

--- lib/main.dart
+++ lib/main.dart
@@ -16,7 +16,7 @@ class MyApp extends StatelessWidget {
         appBar: AppBar(
           title: const Text('Selamat datang Flutter'),
         ),
-        body: Center(
+        body: const Center(
           child: Text('Halo dunia'),
         ),
       ),
